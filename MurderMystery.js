a118435d93fb33446d23ffa6f8a983fd0a3436b0
Furqan <nblogist1@gmail.com>
2019-08-02 20:58:13 +0500

    The bankaccount works fine but Udacity isn't accepting it

---
+++ MurderMystery.js
@@ -0,0 +1,31 @@
+/*
+ * Programming Quiz: Murder Mystery (3-4)
+ */
+
+// change the value of `room` and `suspect` to test your code
+var room = "billiards room";
+var suspect = "Mr. Parkes";
+
+var weapon = "";
+var solved = false;
+
+if (room === "dining room" && suspect === "Mr. Parkes") {
+    weapon = "knife";
+    solved = true;
+} else if (room === "billiards room" && suspect === "Mrs. Sparr") {
+    weapon = "pool stick";
+    solved = true;
+} else if (room === "gallery" && suspect === "Ms. Van Cleve") {
+    weapon = "trophy";
+    solved = true;
+} else  if (room === "ballroom" && suspect === "Mr. Kalehoff") {
+    weapon = "poison";
+    solved = true;
+} else {
+    solved=false;
+    console.log("Mystery remains unsolved!");
+}
+
+if (solved) {
+	console.log(suspect+" did it in the "+room+" with the "+weapon+"!");
+}
