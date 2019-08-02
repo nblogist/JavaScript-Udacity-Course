4d9db8a5e94496e26d227e2b7bf584f6c4c9c5ef
Furqan <nblogist1@gmail.com>
2019-08-02 20:03:31 +0500

    Completed Different Quizes

---
+++ HammerFlowChart.js
@@ -0,0 +1,8 @@
+var price = 10.00; // price of hammer
+var money = 50.00; //Money I have
+
+if (money>=price) {
+    console.log("Buy the hammer Dude"); 
+} else {
+    console.log("Don/'t buy the hammer");
+}
